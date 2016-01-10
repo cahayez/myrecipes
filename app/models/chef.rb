06dfc0f454e1e7a182ba2f51d67b011b737e6fb1
Curtis Hayes <cahayez@gmail.com>
2016-01-10 23:20:02 +0000

    create associations between chefs and recipes, and added to test suite

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,6 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
+  before_save { self.email = email.downcase } #before_save callback
   validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 105},
